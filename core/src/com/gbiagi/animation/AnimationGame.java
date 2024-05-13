99fe53d1c77befd3cc4822d8a87a8775ef5d79e7
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2024-05-13 16:11:50 +0200

    animation fix

--- core/src/com/gbiagi/animation/AnimationGame.java
+++ core/src/com/gbiagi/animation/AnimationGame.java
@@ -1,88 +1,36 @@
 package com.gbiagi.animation;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class AnimationGame extends ApplicationAdapter {
-	// Constant rows and columns of the sprite sheet
-	private static final int FRAME_COLS = 8, FRAME_ROWS = 2;
-	private float spriteWidth = 2f;
-	private float spriteHeight = 2f;
-	private int posX = 50;
-	private int posY = 50;
-	// Objects used
-	Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
-	Texture walkSheet;
-	SpriteBatch spriteBatch;
+public class AnimationGame extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
 
-	// A variable for tracking elapsed time for the animation
-	float stateTime;
-
+	public OrthographicCamera camera;
 	@Override
 	public void create() {
-
-		// Load the sprite sheet as a Texture
-		walkSheet = new Texture(Gdx.files.internal("scottpilgrim_multiple.png"));
-
-		// Use the split utility method to create a 2D array of TextureRegions. This is
-		// possible because this sprite sheet contains frames of equal size and they are
-		// all aligned.
-		TextureRegion[][] tmp = TextureRegion.split(walkSheet,
-				walkSheet.getWidth() / FRAME_COLS,
-				walkSheet.getHeight() / FRAME_ROWS);
-
-		// Place the regions into a 1D array in the correct order, starting from the top
-		// left, going across first. The Animation constructor requires a 1D array.
-		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS]; // Only need frames from one row
-		int index = 0;
-		int firstRow = FRAME_ROWS - 2; // Index of the first row
-		for (int i = 0; i < FRAME_COLS; i++) {
-			walkFrames[index++] = tmp[firstRow][i];
-		}
-
-		// Initialize the Animation with the frame interval and array of frames
-		walkAnimation = new Animation<TextureRegion>(0.08f, walkFrames);
-
-		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
-		// time to 0
-		spriteBatch = new SpriteBatch();
-		stateTime = 0f;
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		this.setScreen(new Animator());
 	}
-
-	@Override
 	public void render() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
-		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
-
-		// Get current frame of animation for the current stateTime
-		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
-		spriteBatch.begin();
-		spriteBatch.draw(currentFrame, posX, posY, currentFrame.getRegionWidth() * spriteWidth, currentFrame.getRegionHeight() * spriteHeight);
-		spriteBatch.end();
+		super.render(); // important!
 	}
 
-	@Override
-	public void pause() {
-
-	}
-	@Override
-	public void resume() {
-
-	}
-	@Override
-	public void resize(int width, int height) {
-		spriteWidth = width;
-		spriteHeight = height;
-	}
-	@Override
-	public void dispose() { // SpriteBatches and Textures must always be disposed
-		spriteBatch.dispose();
-		walkSheet.dispose();
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 }
--- core/src/com/gbiagi/animation/Animator.java
+++ core/src/com/gbiagi/animation/Animator.java
@@ -1,6 +1,6 @@
 package com.gbiagi.animation;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -8,76 +8,66 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class Animator implements ApplicationListener {
+public class Animator implements Screen {
 
-    // Constant rows and columns of the sprite sheet
     private static final int FRAME_COLS = 8, FRAME_ROWS = 2;
 
-    // Objects used
-    Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
+    Animation<TextureRegion> walkAnimation;
     Texture walkSheet;
     SpriteBatch spriteBatch;
 
-    // A variable for tracking elapsed time for the animation
     float stateTime;
 
     @Override
-    public void create() {
-
-        // Load the sprite sheet as a Texture
+    public void show() {
         walkSheet = new Texture(Gdx.files.internal("scottpilgrim_multiple.png"));
 
-        // Use the split utility method to create a 2D array of TextureRegions. This is
-        // possible because this sprite sheet contains frames of equal size and they are
-        // all aligned.
         TextureRegion[][] tmp = TextureRegion.split(walkSheet,
                 walkSheet.getWidth() / FRAME_COLS,
                 walkSheet.getHeight() / FRAME_ROWS);
 
-        // Place the regions into a 1D array in the correct order, starting from the top
-        // left, going across first. The Animation constructor requires a 1D array.
-        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS]; // Only need frames from one row
+        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS];
         int index = 0;
-        int firstRow = FRAME_ROWS - 2; // Index of the first row
+        int firstRow = FRAME_ROWS - 2;
         for (int i = 0; i < FRAME_COLS; i++) {
             walkFrames[index++] = tmp[firstRow][i];
         }
 
-        // Initialize the Animation with the frame interval and array of frames
         walkAnimation = new Animation<TextureRegion>(0.08f, walkFrames);
 
-        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
-        // time to 0
         spriteBatch = new SpriteBatch();
         stateTime = 0f;
     }
 
     @Override
-    public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
-        stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stateTime += Gdx.graphics.getDeltaTime();
 
-        // Get current frame of animation for the current stateTime
         TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
         spriteBatch.begin();
-        spriteBatch.draw(currentFrame, 50, 50); // Draw current frame at (50, 50)
+        spriteBatch.draw(currentFrame, 50, 50);
         spriteBatch.end();
     }
 
     @Override
-    public void pause() {
-
+    public void resize(int width, int height) {
     }
+
+    @Override
+    public void pause() {
+    }
+
     @Override
     public void resume() {
-
     }
-    @Override
-    public void resize(int width, int height) {
 
-    }
     @Override
-    public void dispose() { // SpriteBatches and Textures must always be disposed
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
         spriteBatch.dispose();
         walkSheet.dispose();
     }
